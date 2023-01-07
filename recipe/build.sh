1e470e3e7a2293aef060f5851ebec920ccce05e4
Matthias Diener <mdiener@illinois.edu>
2023-01-07 10:57:37 +0100

    cleanups

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,11 @@
 
 set -ex
 
-echo "$CC $CFLAGS" >> compilers/default
+# From https://cpucycles.cr.yp.to/install.html --
+# Compilers tried are listed in compilers/default. Each compiler includes -fPIC to
+# create a shared library, -fvisibility=hidden to hide non-public symbols in the
+# library, and -fwrapv to switch to a slightly less dangerous version of C.
+echo "$CC $CFLAGS -fwrapv -fvisibility=hidden" > compilers/default
 
 ./configure --prefix=$PREFIX
 
@@ -10,4 +14,5 @@ make
 
 make install
 
+# Remove static libs
 rm $PREFIX/lib/*.a
